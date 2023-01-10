4f3267f944ef8d1919cd17c54fbdd42860760822
Cmethia <celia.methia@gmail.com>
2023-01-10 10:02:46 -0500

    pushing first time

--- src/test/java/com/Automation/code/AUTOMATION/training.java
+++ src/test/java/com/Automation/code/AUTOMATION/training.java
@@ -1,6 +1,9 @@
 package com.Automation.code.AUTOMATION;
 
+import org.openqa.selenium.WebDriver;
+
 public class training {
+	public static WebDriver driver;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
